dc5315e375a822984a800e1d5433b357753fd206
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-07-31 19:01:43 +0545

    Basic Setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Hero } from "@/components";
+
+export default function Home() {
+  return (
+    <main className="over-hidden">
+     <Hero />
+    </main>
+  );
+}
