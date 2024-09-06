54740477149ad2c88a149796d49c75798d9b44dd
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-09-06 22:44:55 +0545

    Working on CarCard Component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -17,8 +17,10 @@ const CarCard = ({ car }: CarCardProps) => {
 
   return (
     <div className='car-card group'>
-        <div className='car-card__content-title'>
-            <h2>{make} {model}</h2>
+        <div className='car-card__content'>
+            <h2 className='car-card__content-title'>
+                {make} {model}
+            </h2>
         </div>
 
         <p className='flex mt-6 text-[32px] font-extrabold'>
@@ -34,12 +36,44 @@ const CarCard = ({ car }: CarCardProps) => {
         <div className='relative w-full h-40 my-3 object-contain'>
             <Image 
                 src="/hero.png" 
-                width={50} 
-                height={50} 
+                // width={50} 
+                // height={50} 
                 alt='car model' 
                 fill priority 
                 className='object-contain'
             />
+        </div>
+
+        <div className='relative flex w-full mt-2'>
+            <div className='flex group-hover:invisible w-full justify-between text-gray'>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src='/steering-wheel.svg' width={20}
+                        height={20} alt='steering wheel' />
+                        { transmission === 'a' ? 'Automatic' : 'Manual' }
+                </div>
+            </div>
+        </div>
+
+        <div className='relative flex w-full mt-2'>
+            <div className='flex group-hover:invisible w-full justify-between text-gray'>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src='/tire.svg' width={20}
+                        height={20} alt='tire' />
+                        <p className='text-[14px]'>
+                            {drive.toUpperCase()}
+                        </p>
+                </div>
+            </div>
+        </div>
+
+        <div className='relative flex w-full mt-2'>
+            <div className='flex group-hover:invisible w-full justify-between text-gray'>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src='/steering-wheel.svg' width={20}
+                        height={20} alt='steering wheel' />
+                        { transmission === 'a' ? 'Automatic' : 'Manual' }
+                </div>
+            </div>
         </div>
     </div>
   )
