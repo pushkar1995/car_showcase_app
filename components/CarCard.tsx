f0e740440767989d72cbcb0394539ba1d845c172
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-09-13 15:04:06 +0545

    CarCard component design ready

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -13,6 +13,8 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
     const { city_mpg, year, make, model, transmission, drive } = car;
 
+    const [isOpen, setIsOpen] = useState(false)
+
     const carRent = calculateCarRent(city_mpg, year)
 
   return (
@@ -35,9 +37,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
         <div className='relative w-full h-40 my-3 object-contain'>
             <Image 
-                src="/hero.png" 
-                // width={50} 
-                // height={50} 
+                src="/hero.png"  
                 alt='car model' 
                 fill priority 
                 className='object-contain'
@@ -49,13 +49,11 @@ const CarCard = ({ car }: CarCardProps) => {
                 <div className='flex flex-col justify-center items-center gap-2'>
                     <Image src='/steering-wheel.svg' width={20}
                         height={20} alt='steering wheel' />
-                        { transmission === 'a' ? 'Automatic' : 'Manual' }
+                        <p className='text-[14px]'>
+                         { transmission === 'a' ? 'Automatic' : 'Manual' }
+                        </p>
                 </div>
-            </div>
-        </div>
 
-        <div className='relative flex w-full mt-2'>
-            <div className='flex group-hover:invisible w-full justify-between text-gray'>
                 <div className='flex flex-col justify-center items-center gap-2'>
                     <Image src='/tire.svg' width={20}
                         height={20} alt='tire' />
@@ -63,16 +61,21 @@ const CarCard = ({ car }: CarCardProps) => {
                             {drive.toUpperCase()}
                         </p>
                 </div>
-            </div>
-        </div>
 
-        <div className='relative flex w-full mt-2'>
-            <div className='flex group-hover:invisible w-full justify-between text-gray'>
                 <div className='flex flex-col justify-center items-center gap-2'>
-                    <Image src='/steering-wheel.svg' width={20}
+                    <Image src='/gas.svg' width={20}
                         height={20} alt='steering wheel' />
-                        { transmission === 'a' ? 'Automatic' : 'Manual' }
+                        {city_mpg} MPG
                 </div>
+            </div>
+            <div className='car-card__btn-container'>
+                <CustomButton
+                    title='View More'
+                    containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+                    textStyles='text-white text-[14px] leading-[14px] font-bold'
+                    rightIcon='/right-arrow.svg'
+                    handleClick={() => setIsOpen(true)} 
+                />
             </div>
         </div>
     </div>
